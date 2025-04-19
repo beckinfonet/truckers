0a659e1e1fe0339c8092c70cf3c6e91e3eb6d161
Beck Maldin <beckprograms@gmail.com>
2025-04-19 01:22:27 -0700

    path resolution

--- app/auth/onboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// import BrokerOnboarding from "./(broker)/page";
-import CarrierOnboarding from "./(carrier)/page";
-
-export default function Onboard() {
-  return <CarrierOnboarding />;
-}
